2754a4a042b77e6c0d3b3c43a45e49c8d16a1570
rontykat <rontykat@gmail.com>
2025-01-13 16:25:15 -0800

    Lecture 1/13 Scene Tutorial

---
+++ CP-Making-a-Scene-Template-master/src/main.js
@@ -0,0 +1,12 @@
+// Code Practice: Making a Scene
+// Name: Ronitt Katkoria
+// Date: 1/13/2025
+
+"use strict"
+
+let config = {
+    type: Phaser.AUTO,
+    scene: [ MainMenu, Play ]
+}
+
+let game = new Phaser.Game(config)
